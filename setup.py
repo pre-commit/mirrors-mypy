5cf22ccb774a8be8f47dfe4c1e8c4f177c608cbf
Anthony Sottile <asottile@umich.edu>
2021-10-04 19:59:24 -0400

    Mirror: 0.910

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(
-    name='pre_commit_dummy_package',
+    name='pre_commit_placeholder_package',
     version='0.0.0',
     install_requires=['mypy==0.910'],
 )
